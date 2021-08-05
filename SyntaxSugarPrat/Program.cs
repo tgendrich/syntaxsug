ac5b6d3d55810ac5c6d5c2db569e2c66c071f4b7
tgendrich <gendricht@gmail.com>
2021-08-04 20:09:22 -0500

    end

---
+++ SyntaxSugarPrat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugarPrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var answer = 9;                // if true                     if false
+            var reponse = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9 ";
+            Console.WriteLine(reponse);
+
+        }
+    }
+}
